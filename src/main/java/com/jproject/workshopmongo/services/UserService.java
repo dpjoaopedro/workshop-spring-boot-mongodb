31ca424c08d3c354e97d7764f3a81aafce08e208
Joao <dp.joaopedro@gmail.com>
2020-05-06 16:51:39 -0300

    Atualizacao de usuario com put

--- src/main/java/com/jproject/workshopmongo/services/UserService.java
+++ src/main/java/com/jproject/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.jproject.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,6 +40,18 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		var newObj = repository.findById(user.getId()).get();
+		updateData(newObj, user);
+		return repository.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User user) {
+		newObj.setName(user.getName());
+		newObj.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO dto) {
 		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
