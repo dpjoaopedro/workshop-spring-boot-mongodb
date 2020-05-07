aeae7800aaec6e73c77f8620697f68c44f4e212b
Joao <dp.joaopedro@gmail.com>
2020-05-07 10:43:01 -0300

    Consulta por query method

--- src/main/java/com/jproject/workshopmongo/services/PostService.java
+++ src/main/java/com/jproject/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.jproject.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,9 @@ public class PostService {
 		} 
 		
 		return post.get();
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
